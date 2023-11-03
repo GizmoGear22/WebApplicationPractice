46e43bdeedb71e72aeaa1fac36b82e3b3cb9a3b3
GizmoGear22 <mpsimmer@gmail.com>
2023-11-03 14:33:54 -0400

    initial commit for Razor practice

---
+++ WebApplicationPractice/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplicationPractice.Pages
+{
+	public class IndexModel : PageModel
+	{
+		private readonly ILogger<IndexModel> _logger;
+
+		public IndexModel(ILogger<IndexModel> logger)
+		{
+			_logger = logger;
+		}
+
+		public string message { get; set; }
+
+
+		public void OnPost(string name)
+		{
+			message = name;
+		}
+
+
+
+
+
+	}
+}
